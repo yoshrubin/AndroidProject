1b2316cc5b4315b0ae043d9df53a2f0ae1311120
AvivBentura <avram.962@gmail.com>
2017-02-01 11:26:24 +0200

    started working on agency expendable list view

---
+++ userside/src/main/java/com/example/userside/Backend/expendableList/ChildAgency.java
@@ -0,0 +1,45 @@
+package com.example.userside.Backend.expendableList;
+
+/**
+ * Created by Aviv on 01/02/2017.
+ */
+
+public class ChildAgency {
+    private String agencyName;
+    private String agencyLocation;
+    private String agencyWebsite;
+    private String agencyMail;
+
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public String getAgencyLocation() {
+        return agencyLocation;
+    }
+
+    public void setAgencyLocation(String agencyLocation) {
+        this.agencyLocation = agencyLocation;
+    }
+
+    public String getAgencyWebsite() {
+        return agencyWebsite;
+    }
+
+    public void setAgencyWebsite(String agencyWebsite) {
+        this.agencyWebsite = agencyWebsite;
+    }
+
+    public String getAgencyMail() {
+        return agencyMail;
+    }
+
+    public void setAgencyMail(String agencyMail) {
+        this.agencyMail = agencyMail;
+    }
+}
